1a3c34f4a71318f828ef34229358da6bd75a3268
Sachin Nagila <sachinnagila2053@gmail.com>
2024-05-08 21:47:02 +1000

    dom of break and continue

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const breakExplain = document.getElementsByClassName("break-explain").innerHTML;
-const continueExplain =
-  document.getElementsByClassName("continue-explain").innerHTML;
-const button = document.getElementsByTagName("button");
+// ===========break and continue============
+const breakExplain = document.getElementById("break-explain");
+const continueExplain = document.getElementById("continue-explain");
+const button1 = document.getElementById("button1").innerText;
+const button2 = document.getElementById("button2").innerText;
+let display = 0;
 
 breakExplain.style.display = "none";
+continueExplain.style.display = "none";
 
-const onclick = () => {
-  if (button.innerContext === "Break") {
+function onclick() {
+  if (button1 === "Break") {
+    breakExplain.style.display = "block";
+    return;
+  } else {
+    breakExplain.style.display = "none";
   }
-};
+}
