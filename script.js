5500eee7d40d7b0072d7b33cf04c8386a8b228bd
Sachin Nagila <sachinnagila2053@gmail.com>
2024-05-08 20:41:46 +1000

    break and continue

---
+++ script.js
@@ -0,0 +1,11 @@
+const breakExplain = document.getElementsByClassName("break-explain").innerHTML;
+const continueExplain =
+  document.getElementsByClassName("continue-explain").innerHTML;
+const button = document.getElementsByTagName("button");
+
+breakExplain.style.display = "none";
+
+const onclick = () => {
+  if (button.innerContext === "Break") {
+  }
+};
